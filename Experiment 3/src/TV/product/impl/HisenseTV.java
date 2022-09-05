3ca87d9c832b9545b49311d8057e6c1290b94fa4
huanmengiug <baizhi_ruo@126.com>
2022-09-05 19:11:59 +0800

    HisenseFactory

---
+++ Experiment 3/src/TV/product/impl/HisenseTV.java
@@ -0,0 +1,12 @@
+package TV.product.impl;
+
+import TV.product.TV;
+
+public class HisenseTV implements TV {
+    @Override
+    public void show() {
+        System.out.println("这是一台海信电视");
+
+    }
+
+}
