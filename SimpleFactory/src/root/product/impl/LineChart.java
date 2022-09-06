b2b7364e91a0ff7a46e5a2acfa0afc93ab1349e4
huanmengiug <baizhi_ruo@126.com>
2022-09-06 18:30:47 +0800

    simplefactory

---
+++ SimpleFactory/src/root/product/impl/LineChart.java
@@ -0,0 +1,15 @@
+//designpatterns.simplefactory.LineChart.java
+package root.product.impl;
+
+import root.product.Chart;
+
+//折线图类，充当具体产品类
+public class LineChart implements Chart {
+	public LineChart() {
+		System.out.println("创建折线图！");
+	}
+	
+	public void display() {
+		System.out.println("显示折线图！");
+	}
+}
