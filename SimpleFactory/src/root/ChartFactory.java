36b5e418560bda9bbc6820612838d63c311e59f7
huanmengiug <baizhi_ruo@126.com>
2022-09-06 18:48:07 +0800

    hat

--- SimpleFactory/src/root/ChartFactory.java
+++ SimpleFactory/src/root/ChartFactory.java
@@ -8,18 +8,16 @@ import root.product.impl.PieChart;
 
 //图表工厂类，充当工厂类
 public class ChartFactory {
-    //静态工厂方法
+	// 静态工厂方法
 	public static Chart getChart(String type) {
 		Chart chart = null;
 		if (type.equalsIgnoreCase("histogram")) {
 			chart = new HistogramChart();
 			System.out.println("初始化设置柱状图！");
-		}
-		else if (type.equalsIgnoreCase("pie")) {
+		} else if (type.equalsIgnoreCase("pie")) {
 			chart = new PieChart();
 			System.out.println("初始化设置饼状图！");
-		}
-		else if (type.equalsIgnoreCase("line")) {
+		} else if (type.equalsIgnoreCase("line")) {
 			chart = new LineChart();
 			System.out.println("初始化设置折线图！");
 		}
--- SimpleFactory/src/root/product/impl/HistogramChart.java
+++ SimpleFactory/src/root/product/impl/HistogramChart.java
@@ -8,7 +8,7 @@ public class HistogramChart implements Chart {
 	public HistogramChart() {
 		System.out.println("创建柱状图！");
 	}
-	
+
 	public void display() {
 		System.out.println("显示柱状图！");
 	}
