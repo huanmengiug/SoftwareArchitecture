7a1a73f4fbc2b1976c5f91950f63b438a6413366
huanmengiug <baizhi_ruo@126.com>
2022-09-08 09:17:56 +0800

    fix

--- AbstractFactory/src/root/Units/XMLUtil.java
+++ AbstractFactory/src/root/Units/XMLUtil.java
@@ -12,7 +12,7 @@ public class XMLUtil {
 			DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = dFactory.newDocumentBuilder();
 			Document doc;							
-			doc =builder.parse(new File("src//config.xml")); 
+			doc =builder.parse(new File("src//root//config.xml")); 
 		
 			//获取包含类名的文本结点
 			NodeList nl = doc.getElementsByTagName("className");
