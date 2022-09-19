0f1ed38a8190a8d15758197ec138f7082c8a43b3
huanmengiug <baizhi_ruo@126.com>
2022-09-19 18:13:50 +0800

    client

--- Experiment 6/src/Plane/Client.java
+++ Experiment 6/src/Plane/Client.java
@@ -6,11 +6,9 @@ import Plane.products.plane;
 public class Client {
 
     public static void main(String[] args) throws Exception {
-        producers producers;
-        plane plane;
 
-        producers = (producers)XMLUtil.getBean("producer");
-        plane = (plane)XMLUtil.getBean("plane");
+        producers producers = (producers) XMLUtil.getBean("producer");
+        plane plane = (plane) XMLUtil.getBean("plane");
 
         producers.setPlane(plane);
         producers.product();
