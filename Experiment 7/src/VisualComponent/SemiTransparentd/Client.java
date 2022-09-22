8c42b0b37ddae21b04eea748a04f291dcb5196c3
huanmengiug <baizhi_ruo@126.com>
2022-09-22 15:10:20 +0800

    client

--- Experiment 7/src/VisualComponent/SemiTransparentd/Client.java
+++ Experiment 7/src/VisualComponent/SemiTransparentd/Client.java
@@ -7,18 +7,17 @@ import VisualComponent.Transparentd.Decorator.ConcreteDecorator.ScrollBarDecorat
 
 public class Client {
     public  static void main(String args[]) throws Exception{
-        Component component_o;//使用抽象构件类型定义
-        component_o = new ListBox();
-
+        Component component;//使用抽象构件类型定义
+        component = new ListBox();
 
         BlackBorderDecorator component_b;//使用具体装饰类型定义 
-        component_b = new BlackBorderDecorator(component_o);
+        component_b = new BlackBorderDecorator(component);
         component_b.setBlackBorder();//单独调用新增业务方法
         ScrollBarDecorator component_s;//使用具体装饰类型定义 
-        component_s = new ScrollBarDecorator(component_o);
+        component_s = new ScrollBarDecorator(component);
         component_s.setScrollBar();
 
-        component_o.display();
+        component.display();
 
 
 
