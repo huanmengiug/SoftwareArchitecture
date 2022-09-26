5893e9eac06f74ba7f1e9c8da8eeb5d6f31a2c99
huanmengiug <baizhi_ruo@126.com>
2022-09-26 15:34:28 +0800

    facade

---
+++ Facade/src/EncryptFacade.java
@@ -0,0 +1,12 @@
+
+
+//加密外观类：外观类
+public class EncryptFacade  extends AbstractEncryptFacade {
+
+	@Override
+	public void fileEncrypt(String fileNameSrc, String fileNameDes) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
