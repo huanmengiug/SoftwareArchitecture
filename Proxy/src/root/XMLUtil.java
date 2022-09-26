fe71b02c271936769151a16651110192816f8284
huanmengiug <baizhi_ruo@126.com>
2022-09-26 16:14:40 +0800

    cogfig

--- Proxy/src/root/XMLUtil.java
+++ Proxy/src/root/XMLUtil.java
@@ -11,7 +11,7 @@ public class XMLUtil {
 			DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = dFactory.newDocumentBuilder();
 			Document doc;							
-			doc =builder.parse(new File("src//config.xml")); 
+			doc =builder.parse(new File("src//root//config.xml")); 
 		
 			//获取包含类名的文本节点
 			NodeList nl = doc.getElementsByTagName("className");
