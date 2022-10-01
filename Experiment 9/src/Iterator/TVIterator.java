92d2d0c14db44493391d5e3b51452fd1945aa63b
huanmengiug <baizhi_ruo@126.com>
2022-10-01 16:37:36 +0800

    Iterator

---
+++ Experiment 9/src/Iterator/TVIterator.java
@@ -0,0 +1,11 @@
+package Iterator;
+
+public interface TVIterator {
+    void setChannel(int i);
+	void next();
+	void previous();
+	boolean isLast();
+	Object currentChannel();
+    boolean isFirst();
+    
+}
