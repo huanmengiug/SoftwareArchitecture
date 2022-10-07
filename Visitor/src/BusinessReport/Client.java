2e54ae5b8bffffe666fd5fc35f3ef2b0cb1f02ac
huanmengiug <baizhi_ruo@126.com>
2022-10-07 10:04:27 +0800

    solt

--- Visitor/src/BusinessReport/Client.java
+++ Visitor/src/BusinessReport/Client.java
@@ -1,39 +1,58 @@
 package BusinessReport;
 
-import java.io.IOException;
-import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
+
+import com.alibaba.fastjson.JSON;
 
 import BusinessReport.ObjectStructure.BusinessReport;
 import BusinessReport.Visitor.ConcreteVisitor.CEOVisitor;
 import BusinessReport.Visitor.ConcreteVisitor.CTOVisitor;
-import org.json.simple.JSONObject;
 
 public class Client {
-    public static String data(String Visitor) throws IOException {
-        JSONObject json = new JSONObject();
+    public static String data(String Visitor) {
+        Map<String, List<Map<String, String>>> map = new HashMap<String, List<Map<String, String>>>();
         BusinessReport report = new BusinessReport();
         List<String> bss = report.showReport(new CTOVisitor());
 
         Iterator<String> iterator = bss.iterator();
         while (iterator.hasNext()) {
+            List<Map<String, String>> list = new ArrayList<>();
+            Map<String, String> tmp = new HashMap<>();
             String next = iterator.next();
-            next = next.split(": ")[1];
-            json.put("name",next.split(",")[0]);
+            String lei = next.split(": ", 2)[0];
+            String name_kl = next.split(": ", 2)[1];
+            String name = name_kl.split(",")[0];
+            String kl = name_kl.split(", ")[1];
+            if (!map.containsKey(lei)) {
+                tmp.put("name", name);
+                tmp.put(kl.split(": ")[0], kl.split(": ")[1]);
+                list.add(tmp);
+                map.put(lei, list);
+            } else {
+                
+                list = map.get(lei);
+                tmp.put("name", name);
+                tmp.put(kl.split(": ")[0], kl.split(": ")[1]);
+                list.add(tmp);
+            }
         }
-        StringWriter out = new StringWriter();
-        json.writeJSONString(out);
-        String data = out.toString();
-        return data ;
+        String mapJson = JSON.toJSONString(map);
+        System.out.println(mapJson);
+
+        return mapJson;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         // 构建报表
         BusinessReport report = new BusinessReport();
         System.out.println("=========== CEO看报表 ===========");
         report.showReport(new CEOVisitor());
         System.out.println("=========== CTO看报表 ===========");
         report.showReport(new CTOVisitor());
+        // data("11");
     }
 }
--- Visitor/src/BusinessReport/Visitor/ConcreteVisitor/CTOVisitor.java
+++ Visitor/src/BusinessReport/Visitor/ConcreteVisitor/CTOVisitor.java
@@ -8,12 +8,12 @@ public class CTOVisitor implements Visitor {
     @Override
     public String visit(Engineer engineer) {
         System.out.println("工程师: " + engineer.name + ", 代码行数: " + engineer.getCodeLines());
-        return "工程师: " + engineer.name + ", 代码行数: " + engineer.getCodeLines();
+        return "工程师: " + engineer.name + ", value: " + engineer.getCodeLines();
     }
 
     @Override
     public String visit(Manager manager) {
         System.out.println("经理: " + manager.name + ", 产品数量: " + manager.getProducts());
-        return "经理: " + manager.name + ", 产品数量: " + manager.getProducts();
+        return "经理: " + manager.name + ", value: " + manager.getProducts();
     }
 }
